3c630492939d707956c78e10cd11ba1f262d055d
williamsears <wpeter1@hotmail.com>
2015-11-18 19:11:23 +0000

    added firstandlast function

--- cps.js
+++ cps.js
@@ -18,3 +18,11 @@ function lastLetter(string, ret) {
 lastLetter(word, function(word) {
     console.log(word)
 });
+
+function firstandlast(string, ret) {
+    ret(string[0] + string[string.length -1])
+}
+
+firstandlast(word, function(word) {
+    console.log(word);
+});
