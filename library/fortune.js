5b64488aaa0d083a27ef47c784ffacc21398c3b9
williamsears <wpeter1@hotmail.com>
2015-11-18 20:46:37 +0000

    added function that delivers a random fortune

---
+++ library/fortune.js
@@ -0,0 +1,11 @@
+function a() {
+
+var fortunes = ["Fortune 1", "Fortune 2", "Fortune 3", "Fortune 4", "Fortune 5"];
+var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+
+console.log (randomFortune);
+}
+module.exports  = {
+    getFortune: a
+    
+};
